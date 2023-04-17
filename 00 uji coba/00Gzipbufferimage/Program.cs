bb48cdf75fe3916ac3c49371d463618d5b70af41
dhearizky12 <dherizdhea@gmail.com>
2023-04-17 12:10:50 +0700

    commit ke lima

---
+++ 00 uji coba/00Gzipbufferimage/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputFilePath = "C:/Users/Formulatrix/Desktop/picture-before.png";
+        string outputFilePath = "example-picture1.gz";
+
+        // Set buffer size
+        int bufferSize = 1;
+
+        // Create input and output buffer
+        byte[] inputBuffer = new byte[bufferSize];
+        byte[] outputBuffer;
+
+        // Create output stream
+        using (FileStream outputStream = File.Create(outputFilePath))
+        {
+            // Create GZip stream
+            using (GZipStream gzipStream = new GZipStream(outputStream, CompressionLevel.SmallestSize))
+            {
+                // Read input file in buffer
+                using (FileStream inputStream = File.OpenRead(inputFilePath))
+                {
+                    int bytesRead;
+                    while ((bytesRead = inputStream.Read(inputBuffer, 0, inputBuffer.Length)) > 0)
+                    {
+                        // Compress input buffer to output buffer
+                        using (MemoryStream memoryStream = new MemoryStream())
+                        {
+                            using (GZipStream memoryGZipStream = new GZipStream(memoryStream, CompressionLevel.Optimal))
+                            {
+                                memoryGZipStream.Write(inputBuffer, 0, bytesRead);
+                            }
+                            outputBuffer = memoryStream.ToArray();
+                        }
+
+                        // Write output buffer to GZip stream
+                        gzipStream.Write(outputBuffer, 0, outputBuffer.Length);
+                    }
+                }
+            }
+        }
+
+        // Get size of original file
+        FileInfo originalFile = new FileInfo(inputFilePath);
+        Console.WriteLine($"Original file size: {originalFile.Length} bytes");
+
+        // Get size of compressed file
+        FileInfo compressedFile = new FileInfo(outputFilePath);
+        Console.WriteLine($"Compressed file size: {compressedFile.Length} bytes.");
+
+        Console.WriteLine($"File {inputFilePath} compressed to {outputFilePath}.");
+    }
+}
---
+++ 00 uji coba/00Pertemuan19/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputFilePath = "C:/Users/Formulatrix/Desktop/need purchase lagi.png";
+        //string inputFilePath = "C:/Users/Formulatrix/Desktop/picture-before.png";
+        
+        // Get size of input file
+        FileInfo inputFile = new FileInfo(inputFilePath);
+        long inputFileSize = inputFile.Length;
+        Console.WriteLine($"Input file size: {inputFileSize} bytes.");
+        
+        
+        //string outputFilePath = "output-1.gz";
+        
+        string outputFilePath = "C:/Users/Formulatrix/Desktop/picture-after2.gz";
+
+    
+        
+        // Read input file as byte array
+        byte[] inputBytes = File.ReadAllBytes(inputFilePath);
+        
+        // Create output stream
+        using (FileStream outputStream = File.Create(outputFilePath))
+        {
+            // Create GZip stream
+            using (GZipStream gzipStream = new GZipStream(outputStream, CompressionLevel.Optimal))
+            {
+                // Write byte array to GZip stream
+                gzipStream.Write(inputBytes, 0, inputBytes.Length);
+            }
+        }
+        
+        Console.WriteLine($"File {inputFilePath} compressed to {outputFilePath}.");
+            // Get size of output file
+        FileInfo outputFile = new FileInfo(outputFilePath);
+        long outputFileSize = outputFile.Length;
+        Console.WriteLine($"Output file size: {outputFileSize} bytes.");
+
+    }
+     
+}
